5ae627770aa19a4f9f722238eea5e1492c85aaae
KhayalethuM <161151117+KhayalethuM@users.noreply.github.com>
2025-03-06 19:01:14 +0200

    Add files via upload

---
+++ question_3.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+
+
+
+public class question_3 {
+   
+    public static void anagram(String a, String b){
+       
+       ArrayList<Character> list1 = new ArrayList<>();
+       ArrayList<Character> list2 = new ArrayList<>();
+       for(int i =0; i<a.length();i++){
+      
+        list1.add(a.charAt(i));
+
+
+       }
+       for(int i =0; i<b.length();i++){
+      
+        list2.add(b.charAt(i));
+
+
+       }
+       list1.sort(null);
+       list2.sort(null);
+
+       if(list1.size() != list2.size()){
+        System.out.println("False");
+       }
+
+       else{
+        String boool = "false";
+        
+        for(int i=0; i<list1.size(); i++){
+            if(Character.toLowerCase(list1.get(i)) != Character.toLowerCase(list2.get(i))){
+                
+                break;
+            }
+            boool = "true";
+
+           
+        }
+
+        System.out.println(boool);
+           
+            
+
+
+        }
+
+
+            
+            
+
+        }
+       
+
+
+       
+    
+
+public static void main(String[] args) throws Exception {
+Scanner sc = new Scanner(System.in);
+String a = sc.nextLine();
+String b = sc.nextLine();
+    anagram(a, b);
+    }
+}
