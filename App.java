b3bb25d047de1c769b53663152dabf6d3c06d6a0
KhayalethuM <161151117+KhayalethuM@users.noreply.github.com>
2025-03-06 18:41:06 +0200

    Add files via upload

--- App.java
+++ App.java
@@ -1,77 +1,68 @@
-/*WE receive input in the form name#number..
-we must seperate the name from the number and maybe try to store the two in a data structure which allows us
-to store two variables side by side , sort inside the structure and dispay the things */
-//for(int i=0; i<name_mark.length(); i++){
-   // sb.append(name_mark.charAt(i));
-
-   
 import java.util.*;
 
 
 
-public class App {
-    public static void seperate(ArrayList<String> arr){
-        StringBuilder sb = new StringBuilder();
-        StringBuilder st = new StringBuilder();
-        
-        TreeMap<Integer, String> marks = new TreeMap<>();
 
+public class App {
+   
+    public static void anagram(String a, String b){
+       
+       ArrayList<Character> list1 = new ArrayList<>();
+       ArrayList<Character> list2 = new ArrayList<>();
+       for(int i =0; i<a.length();i++){
       
+        list1.add(a.charAt(i));
+
+
+       }
+       for(int i =0; i<b.length();i++){
+      
+        list2.add(b.charAt(i));
+
+
+       }
+       list1.sort(null);
+       list2.sort(null);
+
+       if(list1.size() != list2.size()){
+        System.out.println("False");
+       }
+
+       else{
+        String boool = "false";
         
-        for(int k =0; k<arr.size(); k++){
-            String input = arr.get(k);
-            sb.setLength(0);
-            st.setLength(0);
-           int j =0;
-        while(input.charAt(j) != '#'){
-            sb.append(input.charAt(j));
-            
-             j++;
+        for(int i=0; i<list1.size(); i++){
+            if(Character.toLowerCase(list1.get(i)) != Character.toLowerCase(list2.get(i))){
+                
+                break;
+            }
+            boool = "true";
+
            
         }
-    
-        String nameOfLeaner = sb.toString(); //convert stringbuilder to normal string
-    
-          //  sbb = sb.toString();
-        while(j <input.length()-1){
-            st.append(input.charAt(j+1));
-            j++;
+
+        System.out.println(boool);
+           
+            
+
+
         }
 
-        String markString = st.toString();
-        int mark = Integer.parseInt(markString);
-        marks.put(mark,nameOfLeaner) ;
 
             
+            
+
         }
-      
+       
 
-        
-        for (Map.Entry<Integer, String> mapEntry : marks.entrySet()) {
-            System.out.println(mapEntry.getValue() + ":" + mapEntry.getKey());
-        }
-    }
-
-
-        
-
-     //   HashMap<String, Integer> map = new HashMap<>();
-    
-    
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        ArrayList<String> arr = new ArrayList<>();
-        System.out.println("Please specify the number of inputs *sighs*, followed by the inputs");
-        int n = scanner.nextInt();
-        scanner.nextLine();
 
        
-        for(int i =0; i<n; i++){
-            String name = scanner.nextLine();
-            arr.add(name);
-        }
-    seperate(arr);
+    
 
+public static void main(String[] args) throws Exception {
+Scanner sc = new Scanner(System.in);
+String a = sc.nextLine();
+String b = sc.nextLine();
+    anagram(a, b);
     }
 }
